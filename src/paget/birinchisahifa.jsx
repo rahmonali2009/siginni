3af4554c6e8fd83bdffbb16a79743cfe7ff1b932
rahmonali2009 <mahamadalivrahmonali@gmail.com>
2023-11-02 11:34:14 +0500

    first commit

---
+++ src/paget/birinchisahifa.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import'../css/birinchisahifa.css'
+export default function birinchisahifa() {
+  return (
+    <div className='q1'>
+<div id='w1'>
+<label id='w2' htmlFor="">Login Form</label>
+<br />
+<input type="button" id='w3' value='Login'  />
+<input type=" button" id='w4' placeholder='Sinin Up'/>
+<br />
+<input type="button" id='w5' placeholder='Email Address' />
+<br />
+<input type="button" id='w6' placeholder='Posswort' />
+<br />
+<label id='w7' htmlFor="">Forgatpossvort?</label>
+<br />
+<input type="button" id='w8' placeholder='Login'/>
+<br />
+<label id='w9' htmlFor="">Not a nember? Sinin nov</label>
+</div>
+    </div>
+  )
+}
+
